dab3c10f8545c8d3a94429dfa06ad4d4a973b745
Giovanni Lagorio <giovanni.lagorio@gmail.com>
2021-05-24 08:08:26 +0200

    add GEF rc

--- update.sh
+++ update.sh
@@ -2,3 +2,23 @@
 
 echo "Updating..."
 
+function add_gef_rc {
+    if [[ -f ~/.gef.rc ]]
+    then
+        echo ">>> GEF rc already present"
+        return 1;
+    fi
+    cat > ~/.gef.rc <<-'GEF_RC'
+	[aliases]
+	dq = hexdump qword
+	dd = hexdump dword
+	dw = hexdump word
+	dc = hexdump byte
+	db = hexdump byte
+	GEF_RC
+    echo ">>> GEF rc added"
+    return 0
+}
+
+add_gef_rc
+
